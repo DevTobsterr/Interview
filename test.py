0c9d1dd09679cafebda893810473f31b26cff803
Toby Killen <itoby24@gmail.com>
2022-08-18 02:24:03 +0100

    Refact or unit tests

---
+++ test.py
@@ -0,0 +1,5 @@
+from validate_string import StringValidation
+
+
+test_string = StringValidation('There is no punctuation in this sentence')
+print(test_string.string_numbers_are_spelt())
--- test_validate_string.py
+++ test_validate_string.py
@@ -1,50 +1,56 @@
-from validate_string import StringValidation
+from main import main
 import pytest
 
 string_validation_test_cases = ("test_input_string, expected_outcome_of_test",
     [
         # Invalid Test Cases - Assert False
-        ('The quick brown fox said "hello Mr. lazy dog".', False), 
-        ('the quick brown fox said “hello Mr lazy dog".', False),
-        ('"The quick brown fox said “hello Mr lazy dog."', False),
-        ('One lazy dog is too few, 12 is too many.', False),
-        ('Are there 11, 12, or 13 lazy dogs?', False),
-        ('There is no punctuation in this sentence', False),
+        (f'The quick brown fox said "hello Mr. lazy dog".', False), 
+        (f'the quick brown fox said “hello Mr lazy dog".', False),
+        (f'"The quick brown fox said “hello Mr lazy dog."', False),
+        (f'One lazy dog is too few, 12 is too many.', False),
+        (f'Are there 11, 12, or 13 lazy dogs?', False),
+        (f'There is no punctuation in this sentence', False),
 
         # Valid Test Cases - Assert True
-        ('The quick brown fox said “hello Mr lazy dog”.', True),
-        ('The quick brown fox said hello Mr lazy dog.', True),
-        ('One lazy dog is too few, 13 is too many.', True), 
-        ('One lazy dog is too few, thirteen is too many.', True), 
-        ('How many "lazy dogs" are there?', True)
+        (f'The quick brown fox said “hello Mr lazy dog”.', True),
+        (f'The quick brown fox said hello Mr lazy dog.', True),
+        (f'One lazy dog is too few, 13 is too many.', True), 
+        (f'One lazy dog is too few, thirteen is too many.', True), 
+        (f'How many "lazy dogs" are there?', True)
     ]
 )
 
+
 @pytest.mark.parametrize(*string_validation_test_cases)
 def test_string_has_even_quotation(test_input_string, expected_outcome_of_test):
-    string_validation = StringValidation(test_input_string)
-    assert string_validation.string_has_even_quotation() == expected_outcome_of_test
+    string_validation = main(test_input_string)
+    assert string_validation == expected_outcome_of_test
+
+# @pytest.mark.parametrize(*string_validation_test_cases)
+# def test_string_has_even_quotation(test_input_string, expected_outcome_of_test):
+#     string_validation = StringValidation(test_input_string)
+#     assert string_validation.string_has_even_quotation() == expected_outcome_of_test
 
 
-@pytest.mark.parametrize(*string_validation_test_cases)
-def test_string_starts_with_capital(test_input_string, expected_outcome_of_test):
-    string_validation = StringValidation(test_input_string)
-    assert string_validation.string_starts_with_capital() == expected_outcome_of_test
+# @pytest.mark.parametrize(*string_validation_test_cases)
+# def test_string_starts_with_capital(test_input_string, expected_outcome_of_test):
+#     string_validation = StringValidation(test_input_string)
+#     assert string_validation.string_starts_with_capital() == expected_outcome_of_test
 
 
-@pytest.mark.parametrize(*string_validation_test_cases)
-def test_string_termination_character(test_input_string, expected_outcome_of_test):
-    string_validation = StringValidation(test_input_string)
-    assert string_validation.string_termination_character() == expected_outcome_of_test
+# @pytest.mark.parametrize(*string_validation_test_cases)
+# def test_string_termination_character(test_input_string, expected_outcome_of_test):
+#     string_validation = StringValidation(test_input_string)
+#     assert string_validation.string_termination_character() == expected_outcome_of_test
 
-@pytest.mark.parametrize(*string_validation_test_cases)
-def test_string_has_single_peroid(test_input_string, expected_outcome_of_test):
-    string_validation = StringValidation(test_input_string)
-    assert string_validation.string_has_single_peroid() == expected_outcome_of_test
+# @pytest.mark.parametrize(*string_validation_test_cases)
+# def test_string_has_single_peroid(test_input_string, expected_outcome_of_test):
+#     string_validation = StringValidation(test_input_string)
+#     assert string_validation.string_has_single_peroid() == expected_outcome_of_test
 
 
-@pytest.mark.parametrize(*string_validation_test_cases)
-def test_string_numbers_are_spelt(test_input_string, expected_outcome_of_test):
-    string_validation = StringValidation(test_input_string)
-    assert string_validation.string_numbers_are_spelt() == expected_outcome_of_test
+# @pytest.mark.parametrize(*string_validation_test_cases)
+# def test_string_numbers_are_spelt(test_input_string, expected_outcome_of_test):
+#     string_validation = StringValidation(test_input_string)
+#     assert string_validation.string_numbers_are_spelt() == expected_outcome_of_test
 
